c5636f96d85accbd672d60df59015a29971b74e1
ibrahim-ethem-ispir <79301986+ibrahim-ethem-ispir@users.noreply.github.com>
2021-02-20 09:56:49 +0300

    Add files via upload

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler Müşteri Kadedildi : " + musteri.Adi);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi  "+musteri.Adi + musteri.Soyadi);
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Musteri listelendi...");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "İbrahim ethem";
+            musteri1.Soyadi = "İspir";
+            musteri1.id = 50468524;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Eren";
+            musteri2.Soyadi = "Kala";
+            musteri2.id = 25489654;
+
+            MusteriManager musteriEkle = new MusteriManager();
+            musteriEkle.MusteriEkle(musteri1);
+            musteriEkle.MusteriEkle(musteri2);
+
+            MusteriManager musterisil = new MusteriManager();
+            musterisil.MusteriSil(musteri2);
+
+            
+            Musteri[] musteriList = new Musteri[] {musteri1,musteri2 };
+            foreach (var musteri in musteriList)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.id);
+            }
+            MusteriManager musteriListe = new MusteriManager();
+            musteriListe.MusteriListele(musteri1);
+            musteriListe.MusteriListele(musteri2);
+        }
+    }
+}
