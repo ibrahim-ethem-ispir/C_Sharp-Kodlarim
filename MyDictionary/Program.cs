613d638879e0f363525f2c68a00a6faa0c407567
ibrahim-ethem-ispir <79301986+ibrahim-ethem-ispir@users.noreply.github.com>
2021-02-27 11:59:36 +0300

    MyDictionary

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Odev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dictionary = new MyDictionary<string, int>();
+            dictionary.add("Masa", 10);
+            dictionary.add("Kitap", 15);
+            dictionary.add("Telefon", 4);
+
+            dictionary.list();
+        }
+    }
+}
